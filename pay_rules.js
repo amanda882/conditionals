7e5e49b4699a487fd1a3fa42d495b7bc792a6430
Amanda Lang <alang@tb.yearup.org>
2022-10-19 14:07:43 -0400

    add video tag, add button, style video, link and style css

---
+++ pay_rules.js
@@ -0,0 +1,12 @@
+let payRate = "12.50";
+console.log(typeof payRate);
+let hoursWorked = "20";
+console.log(typeof hoursWorked);
+let grossPay = hoursWorked * payRate
+console.log(typeof hoursWorked * payRate)
+
+if { time is <40 hoursWorked
+    
+
+}
+
